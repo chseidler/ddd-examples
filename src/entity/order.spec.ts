6f6c8208f32eb78ae4b758123882ebd8f379cb91
Christian Seidler <ch.cseidler@gmail.com>
2023-04-29 13:10:50 -0300

    Order tests

---
+++ src/entity/order.spec.ts
@@ -0,0 +1,41 @@
+import Order from "./order"
+import OrderItem from "./order_item"
+
+describe("Order unit testes", () => {
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            let order = new Order("", "123", [])
+        }).toThrowError("Id is required")
+    })
+
+    it("should throw error when customerId is empty", () => {
+        expect(() => {
+            let order = new Order("123", "", [])
+        }).toThrowError("CustomerId is required")
+    })
+
+    it("should throw error when items is empty", () => {
+        expect(() => {
+            let order = new Order("123", "123", [])
+        }).toThrowError("Items are required")
+    })
+
+    it("should calculate total", () => {
+        const item = new OrderItem("i1", "Item 1", 100);
+        const order = new Order("o1", "c1", [item])
+
+        const total = order.total();
+
+        expect(total).toBe(100)
+    })
+
+    it("should calculate total", () => {
+        const item = new OrderItem("i1", "Item 1", 100);
+        const item2 = new OrderItem("i2", "Item 2", 200);
+        const order = new Order("o1", "c1", [item, item2])
+
+        const total = order.total();
+
+        expect(total).toBe(300)
+    })
+})
